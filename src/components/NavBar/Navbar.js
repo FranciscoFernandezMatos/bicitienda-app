b3f359306a94a89e69fc66b1080b1b02effdb609
Francisco Fernandez <matos@MBPdeFrancisco.fibertel.com.ar>
2022-05-06 20:17:20 -0300

    Entrega Final Proyecto React Coderhouse

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Navbar from "react-bootstrap/NavBar";
+import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
